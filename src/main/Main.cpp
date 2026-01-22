5814443bdb68a6f43c282f3c570acab46289587e
Ifham22 <ifham424@gmail.com>
2026-01-22 21:51:58 +0600

    library functions in main

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,47 +1,22 @@
-// TESTING PURPOSE (FOR NOW)
-// Test 1: Adding data to students.csv file
-// Test 2: Removing data from students.csv file
-// Test 3: Reading data from students.csv file
-
 #include <iostream>
-#include "../models/Admin.h"
+#include <iomanip>
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#include <string>
+#include <sstream>
+#include "../services/FileHandler.h"
+#include "../services/Hasher.h"
+#include "../managers/StudentManager.h"
+#include "../managers/ComplaintManager.h"
+#include "../managers/WorkerManager.h"
+#include "../managers/WorkAssignmentManager.h"
+#include "../managers/EntryExitManager.h"
+#include "../managers/AuthenticationManager.h"
+#include "../managers/DashboardManager.h"
+#include "../managers/RoomManager.h"
+#include "../utils/InputHelper.h"
+#include "../utils/DateTimeHelper.h"
 #include "../models/Student.h"
-
+#include "../models/Complaint.h"
 using namespace std;
-
-int main(){
-    Admin admin;
-    string studentID, name, email, emergency, room;
-    double hallDues;
-
-    // cout << "Student ID: ";
-    // getline(cin, studentID);
-
-    // cout << "Name: ";
-    // getline(cin, name);
-
-    // cout << "Email: ";
-    // getline(cin, email);
-
-    // cout << "Emergency: ";
-    // getline(cin, emergency);
-
-    // cout << "Room: ";
-    // getline(cin, room);
-
-    // cout << "Hall Dues: ";
-    // cin >> hallDues;
-
-    // Student student(studentID, name, email, emergency, room, hallDues);
-
-    // admin.addStudent(student);
-
-    cout << "Enter student ID to remove: ";
-    getline(cin, studentID);
-
-    admin.removeStudent(studentID);
-
-    // admin.viewAllStudents();
-
-    return 0;
-}
