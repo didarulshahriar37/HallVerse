4c4e32e4f806ad3c590cb5c7cd440350fe963190
Ifham22 <ifham424@gmail.com>
2026-01-22 22:01:39 +0600

    main

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -20,3 +20,37 @@
 #include "../models/Student.h"
 #include "../models/Complaint.h"
 using namespace std;
+
+class HallVerseApp {
+private:
+    FileHandler fileHandler;
+    Hasher hasher;
+    StudentManager studentManager;
+    ComplaintManager complaintManager;
+    WorkerManager workerManager;
+    EntryExitManager entryExitManager;
+    AuthenticationManager authManager;
+    WorkAssignmentManager assignmentManager;
+    DashboardManager dashboardManager;
+    RoomManager roomManager;
+
+    std::string currentUserID;
+    bool isAdmin;
+
+    void showWelcome() {
+        std::cout << "\n";
+        std::cout << "╔═══════════════════════════════════════════╗\n";
+        std::cout << "║                                           ║\n";
+        std::cout << "║           WELCOME TO HALLVERSE            ║\n";
+        std::cout << "║    Islamic University of Technology       ║\n";
+        std::cout << "║                                           ║\n";
+        std::cout << "╚═══════════════════════════════════════════╝\n";
+    }
+
+    void showMainMenu() {
+        std::cout << "\n========== MAIN MENU ==========" << "\n";
+        std::cout << "1. Admin Login\n";
+        std::cout << "2. Student Login\n";
+        std::cout << "3. Exit\n";
+        std::cout << "Enter your choice: ";
+    }
