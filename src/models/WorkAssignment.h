d371b03c18e6e886a39fb3abb7f8367f06fe27d0
Ifham22 <ifham424@gmail.com>
2026-01-22 17:58:48 +0600

    update workassignment

--- src/models/WorkAssignment.h
+++ src/models/WorkAssignment.h
@@ -7,16 +7,26 @@ using namespace std;
 class WorkAssignment{
     
     private:
-    string assignmentID;
-    string status;
-    string complaintID;
-    string workerID;
-    string report;
-
+    std::string assignmentID;
+    std::string status;
+    std::string complaintID;
+    std::string workerID;
+    std::string report;
+    
     public:
-    void updateStatus(string status);
-    void addReport(string report);
+    WorkAssignment();
+    WorkAssignment(std::string aID, std::string cID, std::string wID, 
+                   std::string stat, std::string rep);
+    
+    void updateStatus(const std::string& newStatus);
+    void addReport(const std::string& newReport);
     void markCompleted();
+    
+    std::string getAssignmentID() const { return assignmentID; }
+    std::string getComplaintID() const { return complaintID; }
+    std::string getWorkerID() const { return workerID; }
+    std::string getStatus() const { return status; }
+    std::string getReport() const { return report; }
 };
 
 #endif
