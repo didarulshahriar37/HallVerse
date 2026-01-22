aafda6c960936bfcaf3ce5358b362d5c00797b25
Didarul Shahriar <didarul13579@gmail.com>
2026-01-22 18:10:16 +0600

    updated Complaint.cpp with proper methods

--- src/models/Complaint.cpp
+++ src/models/Complaint.cpp
@@ -1,9 +1,26 @@
 #include "Complaint.h"
+using namespace std;
 
-void Complaint::updateStatus(string status){
-
+Complaint::Complaint() {
+    status = "Pending";
 }
 
-void Complaint::viewComplaintDetails(){
+Complaint::Complaint(
+    string complaintID,
+    string category,
+    string description,
+    string status,
+    string date,
+    string studentID
+) {
+    this->complaintID = complaintID;
+    this->category = category;
+    this->description = description;
+    this->status = status;
+    this->date = date;
+    this->studentID = studentID;
+}
 
+void Complaint::updateStatus(const string& newStatus){
+    status = newStatus;
 }
