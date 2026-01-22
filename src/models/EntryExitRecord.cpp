c9b3bc0b65786f4a2140289450791d9af7011ef8
Didarul Shahriar <didarul13579@gmail.com>
2026-01-22 18:54:49 +0600

    updated EntryExitManager.cpp file with proper methods

--- src/models/EntryExitRecord.cpp
+++ src/models/EntryExitRecord.cpp
@@ -1,5 +1,20 @@
 #include "EntryExitRecord.h"
+#include <iostream>
+using namespace std;
 
-void EntryExitRecord::displayRecord(){
-    
+EntryExitRecord::EntryExitRecord(){}
+EntryExitRecord::EntryExitRecord(
+    string recordID,
+    string studentID,
+    string type,
+    string timestamp
+){
+    this->recordID = recordID;
+    this->studentID = studentID;
+    this->type = type;
+    this->timestamp = timestamp;
+}
+
+void EntryExitRecord::displayRecord() const {
+    cout << recordID << " | " << studentID << " | " << type << " | " << timestamp << "\n";
 }
