c0371aa226ef89af89117a4341f0e7ebd192fea2
Ifham22 <ifham424@gmail.com>
2026-01-22 20:47:45 +0600

    complete dash h

--- src/managers/DashboardManager.h
+++ src/managers/DashboardManager.h
@@ -4,3 +4,18 @@
 #include "StudentManager.h"
 #include "ComplaintManager.h"
 #include "EntryExitManager.h" 
+class DashboardManager {
+private:
+    std::string id;
+    StudentManager* studentManager;
+    ComplaintManager* complaintManager;
+    EntryExitManager* entryExitManager;
+    
+public:
+    DashboardManager(StudentManager* sm, ComplaintManager* cm, EntryExitManager* eem);
+    int getTotalStudents();
+    void getComplaintSummary(int& pending, int& inProgress, int& resolved);
+    void displayDashboard();
+};
+
+#endif
