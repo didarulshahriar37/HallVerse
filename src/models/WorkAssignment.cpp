f32e9bd4d58ccc80987010e9cf9adcedd6d8d722
Ifham22 <ifham424@gmail.com>
2026-01-22 18:03:03 +0600

    workAssignment update

--- src/models/WorkAssignment.cpp
+++ src/models/WorkAssignment.cpp
@@ -1,13 +1,21 @@
 #include "WorkAssignment.h"
 
-void WorkAssignment::updateStatus(string status){
+using namespace std;
+WorkAssignment::WorkAssignment() : status("Assigned") {}
 
+WorkAssignment::WorkAssignment(std::string aID, std::string cID, std::string wID, 
+                               std::string stat, std::string rep)
+    : assignmentID(aID), complaintID(cID), workerID(wID), 
+      status(stat), report(rep) {}
+
+void WorkAssignment::updateStatus(const std::string& newStatus) {
+    status = newStatus;
 }
 
-void WorkAssignment::addReport(string report){
-
+void WorkAssignment::addReport(const std::string& newReport) {
+    report = newReport;
 }
 
-void WorkAssignment::markCompleted(){
-    
+void WorkAssignment::markCompleted() {
+    status = "Completed";
 }
