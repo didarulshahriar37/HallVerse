2b4b15df38f3c343a9867be56ac71342e653a943
Muntajim04 <rahman.saimon@yahoo.com>
2026-01-22 20:28:40 +0600

    Some changes to studentmanager.cpp file

--- src/managers/StudentManager.cpp
+++ src/managers/StudentManager.cpp
@@ -1,11 +1,8 @@
 #include "StudentManager.h"
-#include "../models/Student.h"
-#include "../services/FileHandler.h"
-
+#include <iostream>
+ 
 using namespace std;
-
-StudentManager::StudentManager(FileHandler* fileHandler) {
-    this->fileHandler = fileHandler;
+StudentManager::StudentManager(FileHandler* fh) : fileHandler(fh) {
     loadStudents();
 }
 
@@ -16,33 +13,45 @@ void StudentManager::loadStudents() {
 void StudentManager::addStudent(const Student& student) {
     students.push_back(student);
     fileHandler->writeStudents(students);
+    std::cout << "Student added successfully!\n";
 }
 
-void StudentManager::updateStudent(const Student& updatedStudent) {
-    for (size_t i = 0; i < students.size(); i++) {
-        if (students[i].getStudentID() == updatedStudent.getStudentID()) {
-            students[i] = updatedStudent;
+void StudentManager::updateStudent(const Student& student) {
+    for (auto& s : students) {
+        if (s.getStudentID() == student.getStudentID()) {
+            s = student;
             fileHandler->writeStudents(students);
+            std::cout << "Student updated successfully!\n";
             return;
         }
     }
+    std::cout << "Student not found!\n";
 }
 
-void StudentManager::removeStudent(const string& studentID) {
-    for (size_t i = 0; i < students.size(); i++) {
-        if (students[i].getStudentID() == studentID) {
-            students.erase(students.begin() + i);
+void StudentManager::removeStudent(const std::string& studentID) {
+    for (auto it = students.begin(); it != students.end(); ++it) {
+        if (it->getStudentID() == studentID) {
+            students.erase(it);
             fileHandler->writeStudents(students);
+            std::cout << "Student removed successfully!\n";
             return;
         }
     }
+    std::cout << "Student not found!\n";
 }
 
-Student* StudentManager::getStudent(const string& studentID) {
-    for (size_t i = 0; i < students.size(); i++) {
-        if (students[i].getStudentID() == studentID) {
-            return &students[i];
+Student* StudentManager::getStudent(const std::string& studentID) {
+    for (auto& s : students) {
+        if (s.getStudentID() == studentID) {
+            return &s;
         }
     }
     return nullptr;
+}
+
+bool StudentManager::isBedOccupied(const std::string& hall, const std::string& room, const std::string& bed) const {
+    for (const auto& s : students) {
+        if (s.getHallName() == hall && s.getRoomNumber() == room && s.getBedNumber() == bed) return true;
+    }
+    return false;
 }
